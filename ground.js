c59937ec45e0bafca509947a51f858da8b692884
fatin shahriare sium <sium1206@gmail.com>
2021-12-21 17:28:42 +0000

    alhamdulillah,progress furthur

--- ground.js
+++ ground.js
@@ -2,17 +2,22 @@
 let denoGroundElme = document.querySelector('.deno-ground')
 let g1 = document.getElementById('g-1')
 let g2 = document.getElementById('g-2')
+console.log(g2.style);
 
 export function updateDenoGround(delta) {
+    console.log(getComputedStyle(g1).getPropertyValue('left'), getComputedStyle(g2).getPropertyValue('left'));
     ;[...denoGroundElme.children].forEach(sig => {
         if (sig.className == 'deno-ground-img') {
+            let previousLeft = sig.style.left ? parseInt(sig.style.left) : 0
+            //let x = `${sig.style.left ? sig.style.left : 0 + delta * .05}%`
 
-            sig.style.left = `-${delta * .05}%`
-            if (parseInt(g1.style.left) <= -300) {
+            //sig.style.left = `${previousLeft - (delta * .05 * .01)}%`
+            console.log(previousLeft);
+            if (parseInt(sig.style.left) <= -300) {
                 //sig.style.display = 'none'
-                g1.style.left = '600%'
+                sig.style.left = '300%'
+
                 //console.log(sig.style.left, getComputedStyle(sig).getPropertyValue('left'));
-                //console.log(getComputedStyle(g1).getPropertyValue('left'), getComputedStyle(g2).getPropertyValue('left'));
             }
 
         }
