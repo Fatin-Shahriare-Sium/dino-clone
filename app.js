ea32e3b69228864cefbba5bcc7f86911335514c1
fatin shahriare sium <sium1206@gmail.com>
2021-12-24 17:43:19 +0000

    alhamdulillah,able to do the unlimitated ground

--- app.js
+++ app.js
@@ -9,13 +9,12 @@ function updateLoop(time) {
     if (!lastime) {
         lastime = time
         window.requestAnimationFrame(updateLoop)
-
         return
     }
     let delta = time - lastime
     updateDenoGround(delta)
     console.log(time, delta);
-
+    lastime = time
     window.requestAnimationFrame(updateLoop)
 
 }
--- ground.js
+++ ground.js
@@ -5,19 +5,19 @@ let g2 = document.getElementById('g-2')
 console.log(g2.style);
 
 export function updateDenoGround(delta) {
-    console.log(getComputedStyle(g1).getPropertyValue('left'), getComputedStyle(g2).getPropertyValue('left'));
+    console.log(getComputedStyle(g1).getPropertyValue('--left'), getComputedStyle(g2).getPropertyValue('--left'));
     ;[...denoGroundElme.children].forEach(sig => {
         if (sig.className == 'deno-ground-img') {
-            let previousLeft = sig.style.left ? parseInt(sig.style.left) : 0
-            //let x = `${sig.style.left ? sig.style.left : 0 + delta * .05}%`
 
-            //sig.style.left = `${previousLeft - (delta * .05 * .01)}%`
-            console.log(previousLeft);
-            if (parseInt(sig.style.left) <= -300) {
+            let sigElementLeftProperty = parseInt(getComputedStyle(sig).getPropertyValue('--left'))
+
+            sig.style.setProperty('--left', sigElementLeftProperty + (delta * .1 * -1))
+
+            // console.log('sigelement left', sigElementLeftProperty - 30);
+            if (sigElementLeftProperty <= -300) {
+
+                sig.style.setProperty('--left', 300)
                 //sig.style.display = 'none'
-                sig.style.left = '300%'
-
-                //console.log(sig.style.left, getComputedStyle(sig).getPropertyValue('left'));
             }
 
         }
